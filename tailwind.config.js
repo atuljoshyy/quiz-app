5fc7600ca91ac27d42195dcc86f0977babb6603a
AtulJoshyy <atuljoshyy@gmail.com>
2022-01-07 19:56:23 +0530

    🚀 completed the landing page, quiz page UI

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  content: ["./src/**/*.{html,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'main-green': '#218380',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
